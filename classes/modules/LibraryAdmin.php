0f657e8f8c05a3f2257c284a66b3c011f8cb88b5
EA <arrive@free.fr>
2020-01-07 15:32:42 +0100

    refactoring import

--- classes/modules/LibraryAdmin.php
+++ classes/modules/LibraryAdmin.php
@@ -2,6 +2,33 @@
 
 class LibraryAdmin extends StoreAdmin {
     
+    public function publish() {
+        if (isset($this->params['name']) &&
+            isset($this->params['books'])) {
+                $name = $this->params['name'];
+                $books = Zord::objectToArray(json_decode($this->params['books']));
+                (new BookHasContextEntity())->delete([
+                    'many' => true,
+                    'where' => ['context' => $name]
+                ]);
+                foreach ($books as $book) {
+                    if ($book['status'] !== 'del') {
+                        (new BookHasContextEntity())->create([
+                            'book'    => $book['isbn'],
+                            'context' => $name,
+                            'status'  => $book['status']
+                        ]);
+                    } else if ($this->user->isManager()) {
+                        (new BookEntity())->delete($book['isbn'], true);
+                        foreach($this->deletePaths($book['isbn']) as $path) {
+                            Zord::deleteRecursive(DATA_FOLDER.$path);
+                        }
+                    }
+                }
+            }
+            return $this->index('publish');
+    }
+    
     protected function deletePaths($isbn) {
         $metadata = Store::data($isbn, 'meta', 'array');
         $epub = isset($metadata['epub']) ? $metadata['epub'] : $isbn;
