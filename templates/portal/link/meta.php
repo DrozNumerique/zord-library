696bac8ea311efc913870f631d134927294f6bb9
EA <arrive@free.fr>
2020-05-07 15:50:31 +0200

    header meta in library instead of core

---
+++ templates/portal/link/meta.php
@@ -0,0 +1,4 @@
+<?php if (isset($models['portal']['meta']) && is_array($models['portal']['meta'])) { ?>
+	<link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
+	<link rel="unapi-server" type="application/xml" title="unAPI" href="/unapi"/>
+<?php } ?>
