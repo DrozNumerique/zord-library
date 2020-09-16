eefa6b4f11bf5da626a49d6ff1b824c30d51c71e
EA <arrive@free.fr>
2020-09-16 11:03:09 +0200

    generic images for default skin

--- templates/portal/header/left.php
+++ templates/portal/header/left.php
@@ -1,1 +1,1 @@
-			<a href="<?php echo $skin->header->left->url; ?>" class="vendorlink"><img src="<?php echo $skin->header->left->image; ?>"/></a>
+			<a href="<?php echo $skin->header->left->url ?? $baseURL; ?>" class="vendorlink"><img src="<?php echo $skin->header->left->image; ?>"/></a>
