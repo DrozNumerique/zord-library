954668b8f6a81b7cf5039adde95bd22ea4032c05
EA <arrive@free.fr>
2020-10-31 16:39:59 +0100

    add instructions for XML View templates

--- templates/xml/dzi.php
+++ templates/xml/dzi.php
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
 <Image xmlns="http://schemas.microsoft.com/deepzoom/2008"
        Format="<?php echo $models['format']; ?>"
        Overlap="<?php echo $models['overlap']; ?>"
