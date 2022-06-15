a78298d992bda5642b131aa99ef1a813a3faeb09
EA <arrive@free.fr>
2022-06-15 09:47:38 +0200

    add IP in header right

--- templates/portal/header/right.php
+++ templates/portal/header/right.php
@@ -1,4 +1,4 @@
-			<a href="<?php echo $baseURL; ?>" class="contextlink">
+			<a href="<?php echo $baseURL; ?>" class="contextlink" title="<?php echo $user->isConnected() ? $user->name : $_SERVER['REMOTE_ADDR']; ?>">
 <?php $this->render('logo'); ?>
 <?php $this->render('provider'); ?>
 			</a>
