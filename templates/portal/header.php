71ae1dfed32b63392393795bf93831e2f6fcefa5
EA <arrive@free.fr>
2023-04-02 15:14:43 +0200

    use widget switch

--- templates/portal/header.php
+++ templates/portal/header.php
@@ -1,18 +1,8 @@
 	<header id="header">
 		<div class="blocklink">
-<?php
-$this->render('left');
-$this->render('middle');
-$this->render('right');
-?>
+<?php $this->render('left'); ?>
+<?php $this->render('middle'); ?>
+<?php $this->render('right'); ?>
 		</div>
-		<form method="post" id="switchContextForm">
-			<input type="hidden" name="module" value="<?php echo $models['portal']['module']; ?>">
-			<input type="hidden" name="action" value="<?php echo $models['portal']['action']; ?>">
-			<input type="hidden" name="params" value='<?php echo $models['portal']['params']; ?>'>
-			<input type="hidden" name="lang"   value="<?php echo $lang; ?>">
-<?php   if ($user->isConnected()) { ?>
-			<input type="hidden" name="<?php echo User::$ZORD_SESSION; ?>" value="<?php echo $user->session; ?>">
-<?php   } ?>
-		</form>
+<?php $this->render('/portal/widget/switch'); ?>
 	</header>
