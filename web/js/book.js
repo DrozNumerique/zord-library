6534e845409b4837f932f32e72409f9cae8bf26e
EA <arrive@free.fr>
2023-08-16 18:02:26 +0200

    cosmetic

--- web/js/book.js
+++ web/js/book.js
@@ -282,14 +282,12 @@ function displayTEI(selectorIndex) {
 			}
 		//}, 500);
 	}
-	/*
 	[].forEach.call(teiContents, function(teiContent) {
 		var glosses = teiContent.querySelectorAll(GLOSS_SELECTOR);
 		[].forEach.call(glosses, function(gloss) {
 			gloss.style.position = 'absolute';
 		});
 	});
-	*/
 }
 
 (function(undefined) {
