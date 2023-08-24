30ef8dd30c44d566b2ecfb61ecb91a871f108d1c
EA <arrive@free.fr>
2023-08-24 11:24:08 +0200

    cosmetic

--- web/js/book.js
+++ web/js/book.js
@@ -1,11 +1,11 @@
 var TEI_SELECTOR;
-var GLOSS_SELECTOR;
 var PAGE_SELECTOR_INDEX    = 0;
 var LINE_SELECTOR_INDEX    = 1;
 var SECTION_SELECTOR_INDEX = 2;
 var CURRENT_SELECTOR_INDEX = PAGE_SELECTOR_INDEX;
 var ALIGN_SELECTORS;
 var PB_TEMOIN_SELECTOR;
+var MARGIN_GLOSS_SELECTOR;
 
 var els = ['{"nspace":"'];
 for (b = 0; b < IDS.length; b = b + 2) {
@@ -283,11 +283,9 @@ function displayTEI(selectorIndex) {
 		//}, 500);
 	}
 	[].forEach.call(teiContents, function(teiContent) {
-		var glosses = teiContent.querySelectorAll(GLOSS_SELECTOR);
+		var glosses = teiContent.querySelectorAll(MARGIN_GLOSS_SELECTOR);
 		[].forEach.call(glosses, function(gloss) {
-			if (!gloss.parentNode.classList.contains('bibl')) {
-				gloss.style.position = 'absolute';
-			}
+			gloss.style.position = 'absolute';
 		});
 	});
 }
@@ -406,7 +404,6 @@ function displayTEI(selectorIndex) {
 		var pageOld = 0;
 
 		TEI_SELECTOR       = 'div.' + ELS['tei']['elm'];
-		GLOSS_SELECTOR     = 'div.' + ELS['gloss']['elm'];
 		FOOTNOTES_SELECTOR = 'div.footnotes';
 		PAGE_SELECTOR      = 'div.' + ELS['pb']['elm']  + '[data-' + ELS['pb']['n']     + ']:not([data-' + ELS['pb']['rend'] + '="temoin"])';
 		LINE_SELECTOR      = 'div.' + ELS['l']['elm']   + '[data-' + ELS['l']['rend']   + '="margin"]';
@@ -418,6 +415,7 @@ function displayTEI(selectorIndex) {
 		];
 		
 		PB_TEMOIN_SELECTOR = 'div.' + ELS['pb']['elm'] + '[data-' + ELS['pb']['n'] + '][data-' + ELS['pb']['rend'] + '="temoin"]';
+		MARGIN_GLOSS_SELECTOR = 'div.' + ELS['gloss']['elm'] + '[data-' + ELS['gloss']['rend'] + '="margin"]';
 		
 		var pageSelector = 'div.' + ELS['pb']['elm'] + '[data-' + ELS['pb']['n'] + ']';
 		var pageSelectorNot = 'div.' + ELS['pb']['elm'] + '[data-' + ELS['pb']['n'] + ']:not([data-' + ELS['pb']['rend'] + '="temoin"])';
