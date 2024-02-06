b451f7c3fd2872f4a38038cb974a69bf1f1e2fb7
EA <arrive@free.fr>
2024-02-06 17:33:11 +0100

    cosmetic

--- templates/portal/page/home/banner.php
+++ templates/portal/page/home/banner.php
@@ -1,3 +1,3 @@
 		<div id="banner" align="center">
-			<a href="<?php echo Zord::getContextURL($context); ?>"><img src="<?php echo Zord::getSkin($context)->banner->image; ?>"/></a>
+			<a href="<?php echo Zord::getContextURL($context); ?>"><img src="<?php echo Zord::getSkin($context)->banner->image ?? '/public/library/'.$context.'/'.'banner.png'; ?>"/></a>
 		</div>
