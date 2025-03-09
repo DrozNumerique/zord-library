525fc5d122d070ff0c87aa8c84bec799c35fd67a
EA <arrive@free.fr>
2025-03-09 09:51:26 +0100

    cosmetic

--- templates/portal/widget/ariadne.php
+++ templates/portal/widget/ariadne.php
@@ -1,9 +1,9 @@
         <div id="ariadne">
-        	<span class="ariadne-previous" data-part="<?php echo $models['portal']['ariadne']['previous']['link'] ?? ''; ?>" data-id="<?php echo $models['portal']['ariadne']['previous']['id'] ?? ''; ?>" title="<?php echo $this->locale('portal')->ariadne->chapter->previous.' : '.(!empty($models['portal']['ariadne']['previous']['flat']) ? $models['portal']['ariadne']['previous']['flat'] : ($models['portal']['ariadne']['previous']['title'] ?? '')); ?>">
+        	<span class="ariadne-previous" data-part="<?php echo $models['portal']['ariadne']['previous']['link'] ?? ''; ?>" data-id="<?php echo $models['portal']['ariadne']['previous']['id'] ?? ''; ?>" title="<?php echo $this->locale('portal')->ariadne->chapter->previous.' : '.(!empty($models['portal']['ariadne']['previous']['flat']) ? $models['portal']['ariadne']['previous']['flat'] : strip_tags($models['portal']['ariadne']['previous']['title'] ?? '')); ?>">
         		<i class="fa fa-fw fa-chevron-left"></i>
         	</span>
         	<div class="ariadne-content">
-        		<span class="ariadne-book" data-part="<?php echo $models['book']['ISBN'] ?>"><?php echo $models['book']['TITLE'] ?></span>
+        		<span class="ariadne-book" data-part="<?php echo $models['book']['ISBN'] ?>"><?php echo Library::title($models['book']['metadata']['title'], $models['book']['metadata']['subtitle'] ?? null, 100); ?></span>
 <?php if (isset($models['portal']['ariadne']['current']) && $models['book']['PART'] !== 'home') { ?>
         		<span class="ariadne-current" data-part="<?php echo $models['portal']['ariadne']['current']['link'] ?>" data-id="<?php echo $models['portal']['ariadne']['current']['id']; ?>">
         		<?php echo $models['portal']['ariadne']['current']['title']; ?>
@@ -13,7 +13,7 @@
         		</span>
 <?php } ?>
         	</div>
-        	<span class="ariadne-next" data-part="<?php echo $models['portal']['ariadne']['next']['link'] ?? ''; ?>" data-id="<?php echo $models['portal']['ariadne']['next']['id'] ?? ''; ?>" title="<?php echo $this->locale('portal')->ariadne->chapter->next.' : '.(!empty($models['portal']['ariadne']['next']['flat']) ? $models['portal']['ariadne']['next']['flat'] : ($models['portal']['ariadne']['next']['title'] ?? '')); ?>">
+        	<span class="ariadne-next" data-part="<?php echo $models['portal']['ariadne']['next']['link'] ?? ''; ?>" data-id="<?php echo $models['portal']['ariadne']['next']['id'] ?? ''; ?>" title="<?php echo $this->locale('portal')->ariadne->chapter->next.' : '.(!empty($models['portal']['ariadne']['next']['flat']) ? $models['portal']['ariadne']['next']['flat'] : strip_tags($models['portal']['ariadne']['next']['title'] ?? '')); ?>">
         		<i class="fa fa-fw fa-chevron-right"></i>
         	</span>
         </div>
