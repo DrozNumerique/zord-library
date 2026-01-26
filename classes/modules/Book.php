9ecab2b33dd076c4418619563a286d72a5613eb3
EA <arrive@free.fr>
2026-01-26 14:03:23 +0100

    cosmetic

--- classes/modules/Book.php
+++ classes/modules/Book.php
@@ -1027,7 +1027,8 @@ class Book extends Module {
         $order = $shelves;
         $keys = array_keys($labels ?? []);
         $keys[-1]   = 'new';
-        $keys[9999] = 'other';
+        $keys[9998] = 'other';
+        $keys[9999] = 'others';
         usort($order, function($first, $second) use ($keys) {
             $x = (int) array_search($first['name'], $keys);
             $y = (int) array_search($second['name'], $keys);
