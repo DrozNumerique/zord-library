9355b4594161632f694dbcb6a68316c4d3cdaa0d
EA <arrive@free.fr>
2026-09-15 15:09:33 +0200

    cosmetic

--- templates/portal/page/admin/publish/books/epub.php
+++ templates/portal/page/admin/publish/books/epub.php
@@ -1,4 +1,4 @@
-<?php if (file_exists(STORE_FOLDER.'epub'.DS.(Library::data($book, 'metadata.json', 'array')['epub'] ?? 'unknown').'.pdf')) { ?>
+<?php if (file_exists(STORE_FOLDER.'epub'.DS.(Library::data($book, 'metadata.json', 'array')['epub'] ?? 'unknown').'.epub')) { ?>
                         		<td class="epub" data-action="epub" data-isbn="<?php echo $book; ?>" data-context="<?php echo $context; ?>">
           							<i class="display fa fa-book fa-fw" title="<?php echo $locale->tab->publish->epub; ?>"></i>
                         		</td>
